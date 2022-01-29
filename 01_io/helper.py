f65f7ef22fd263e69f130f8163946cd4bc898d3c
ceba <ceba_robot@outlook.com>
2022-01-30 03:44:08 +0800

    fix: add stop instruct

--- 01_io/helper.py
+++ 01_io/helper.py
@@ -5,35 +5,37 @@ for x in range(128):
         y = "45"
     else:
         y = "55"
-    print("%04o@ +%s %04o %04o" % (x, y, 0o1000 + x, 0o2000 + x))
+    print("%04o@ +%s %04o %04o" % (x, y, 0o2000 + x, 0o3000 + x))
 
 for x in range(128):
     if (x % 2):
         y = "45"
     else:
         y = "55"
-    print("%04o@ +%s %04o %04o" % (128 + x, y, 0o2000 + x, 0o2000 + x))
+    print("%04o@ +%s %04o %04o" % (128 + x, y, 0o3000 + x, 0o3000 + x))
 
 for x in range(128):
     if (x % 2):
         y = "07"
     else:
         y = "27"
-    print("%04o@ +%s %04o %04o" % (256 + x, y, 0o0000, 0o1000 + x))
+    print("%04o@ +%s %04o %04o" % (256 + x, y, 0o0000, 0o2000 + x))
 
 for x in range(128):
     if (x % 2):
         y = "45"
     else:
         y = "55"
-    print("%04o@ +%s %04o %04o" % (384 + x, y, 0o1000 + x, 0o1000 + x))
+    print("%04o@ +%s %04o %04o" % (384 + x, y, 0o2000 + x, 0o2000 + x))
+
+print("1000@ +04 0000 0000")
 
 for x in range(128):
     if (x % 2):
         y = "+"
     else:
         y = "-"
-    print("%04o@ %s%010o" % (0o1000 + x, y, random.randint(0, 0o7777777777)))
+    print("%04o@ %s%010o" % (0o2000 + x, y, random.randint(0, 0o7777777777)))
 
 print("")
 
@@ -42,5 +44,5 @@ for x in range(128):
         y = "+"
     else:
         y = "-"
-    print("%s%010o" % (y, random.randint+(0, 0o7777777777)))
+    print("%s%010o" % (y, random.randint(0, 0o7777777777)))
 
